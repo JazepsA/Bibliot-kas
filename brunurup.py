e875a0f3de28eec61b2833ef8bd459860dd10f3a
JazepsA <jazepsaugustans26@gmail.com>
2024-09-24 11:02:55 +0300

    bibliotekas

---
+++ brunurup.py
@@ -0,0 +1,10 @@
+import turtle
+window=turtle.Screen()
+window.bgcolor('navy')
+window.setup(500,400)
+window.title("Turtle")
+window.delay(100000)
+turtle=turtle.Turtle('turtle')
+turtle.color('brown')
+turtle.shapesize(3)
+turtle.showturtle()
---
+++ termcolor2.py
@@ -0,0 +1,6 @@
+from termcolor2 import c 
+
+print(c("Some text").blue.on_white.underline)
+print(c("Datorika").underline)
+print(c("Jauna rindkopa").red)
+print()
